ba1e4d5dff5f90a2130dc80270b43d66fd92d143
kingspp <kingspprathyush@gmail.com>
2020-08-26 11:18:50 -0400

    Updated dashboard

--- app/frontend/src/views/main/Dashboard.vue
+++ app/frontend/src/views/main/Dashboard.vue
@@ -7,10 +7,50 @@
             <v-card-text>
                 <v-row>
                     <v-col class="text-center" cols="4">
+
                         <div>
-                            <img id="clip" :src="currentImage"/>
+                            <img id="clip" :src="currentImage.src"/>
                             <!--                            <div id="rectangle"></div>-->
                         </div>
+                        <div class="text-left" style="padding: 20px;">
+                            <h2 style="padding-bottom: 10px">Details:</h2>
+                            <table class="tg" >
+                                <thead>
+                                <tr>
+                                    <th class="tg-1wig">Image ID: </th>
+                                    <th class="tg-0lax">{{currentImage.imageId}}</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td class="tg-1wig">Experiment: </td>
+                                    <td class="tg-0lax">{{currentImage.experiment}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="tg-1wig">Run: </td>
+                                    <td class="tg-0lax">{{currentImage.runid}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="tg-1wig">Epoch: </td>
+                                    <td class="tg-0lax">{{currentImage.epoch}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="tg-1wig">Step: </td>
+                                    <td class="tg-0lax">{{currentImage.step}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="tg-1wig">Batch: </td>
+                                    <td class="tg-0lax">{{currentImage.batch}}</td>
+                                </tr>
+                                <tr>
+                                    <td class="tg-1wig">Timestamp: </td>
+                                    <td class="tg-0lax">{{currentImage.timestamp}}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div>
+
                     </v-col>
                     <v-col>
                         <v-form ref="form" v-for="characterAnnotation in characterAnnotations"
@@ -69,22 +109,137 @@
     data() {
       return {
         characterAnnotations: [
-          { id: uniqueId(), character: "", probability: 100, clarity: 100, showDelete: false, showAdd: true },
+          {
+            id: uniqueId(),
+            character: "",
+            probability: 100,
+            clarity: 100,
+            showDelete: false,
+            showAdd: true,
+          },
         ],
-        currentImage: require("@/assets/im1.png"),
+        currentImage: {
+          src: require("@/assets/im1.png"),
+          imageId: "1",
+          timestamp: "17:30 24 Aug, 2020",
+          experiment: "Exp_08_032_128_10",
+          runid: "5",
+          epoch: "3",
+          step: "399",
+          batch: "1394",
+        },
         currentImageCounter: 0,
         imageFiles: [
-          require("@/assets/im1.png"),
-          require("@/assets/im2.png"),
-          require("@/assets/im3.png"),
-          require("@/assets/im4.png"),
-          require("@/assets/im5.png"),
-          require("@/assets/im6.png"),
-          require("@/assets/im7.png"),
-          require("@/assets/im8.png"),
-          require("@/assets/im9.png"),
-          require("@/assets/im10.png"),
-          require("@/assets/im11.png"),
+          {
+            src: require("@/assets/im1.png"),
+            imageId: "1",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im2.png"),
+            imageId: "2",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im3.png"),
+            imageId: "3",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im4.png"),
+            imageId: "4",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im5.png"),
+            imageId: "5",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im6.png"),
+            imageId: "6",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im7.png"),
+            imageId: "7",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im8.png"),
+            imageId: "8",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im9.png"),
+            imageId: "9",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im10.png"),
+            imageId: "10",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
+          {
+            src: require("@/assets/im11.png"),
+            imageId: "11",
+            timestamp: "17:30 24 Aug, 2020",
+            experiment: "Exp_08_032_128_10",
+            runid: "5",
+            epoch: "3",
+            step: "399",
+            batch: "1394",
+          },
         ],
       };
     },
@@ -135,7 +290,7 @@
       });
       this.characterAnnotations = [
         { id: uniqueId(), character: "", probability: 100, clarity: 100, showDelete: false, showAdd: true },
-      ]
+      ];
     }
 
     get greetedUser() {
@@ -168,12 +323,12 @@
 
     #clip {
         position: relative;
-        max-width: 250px;
-        max-height: 150px;
+        max-width: 28px;
+        max-height: 28px;
         /*clip: rect(0, 30px, 30px, 0);*/
         /*background-repeat: no-repeat;*/
         /*background-size: 300px 100px;*/
-        zoom: 200%;
+        /*zoom: 100%;*/
         /* clip: shape(top, right, bottom, left); NB 'rect' is the only available option */
     }
 
@@ -181,5 +336,13 @@
         font-size: 2em;
         padding: 10px
     }
+
+    .tg  {border-collapse:collapse;border-color:black;border-spacing:0;border-style:solid;border-width:0px; line-height: 0;}
+    .tg td{border-style:solid;border-width:0px;font-family:Arial, sans-serif;font-size:14px;overflow:hidden;
+        padding:10px 5px;word-break:normal;}
+    .tg th{border-style:solid;border-width:0px;font-family:Arial, sans-serif;font-size:14px;font-weight:normal;
+        overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+    .tg .tg-1wig{font-weight:bold;text-align:left;vertical-align:top}
 
 </style>
