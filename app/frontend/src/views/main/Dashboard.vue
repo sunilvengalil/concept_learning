010d8c0eea5077f1b95fee8c41ce31eb4c5505c6
kingspp <kingspprathyush@gmail.com>
2020-08-25 23:51:15 -0400

    Added support for Dynamic Insertion and deletion of character settings

--- app/frontend/src/views/main/Dashboard.vue
+++ app/frontend/src/views/main/Dashboard.vue
@@ -9,115 +9,104 @@
                     <v-col class="text-center" cols="4">
                         <div>
                             <img id="clip" src="@/assets/im_1.png"/>
-<!--                            <div id="rectangle"></div>-->
+                            <!--                            <div id="rectangle"></div>-->
                         </div>
                     </v-col>
                     <v-col>
                         <v-row><h2>Setting</h2></v-row>
-                        <v-form ref="form">
+                        <v-form ref="form" v-for="characterAnnotation in characterAnnotations"
+                                :key="characterAnnotation.id">
                             <v-row>
                                 <v-col cols="2">
-                                    <v-text-field v-model="first" label="Character" filled></v-text-field>
+                                    <v-text-field v-model="characterAnnotation.character" label="Character" filled></v-text-field>
                                 </v-col>
                                 <v-col>
                                     <v-slider
-                                            v-model="max"
+                                            v-model="characterAnnotation.probability"
                                             label="Probability"
                                             thumb-label="always"
                                     ></v-slider>
                                 </v-col>
 
                                 <v-col>
-                                    <v-slider v-model="max" label="Clarity" thumb-label="always">
+                                    <v-slider v-model="characterAnnotation.clarity" label="Clarity" thumb-label="always">
                                     </v-slider>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols="2">
-                                    <v-text-field v-model="first" label="Character" filled></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-slider
-                                            v-model="max"
-                                            label="Probability"
-                                            thumb-label="always"
-                                    ></v-slider>
                                 </v-col>
 
                                 <v-col>
-                                    <v-slider v-model="max" label="Clarity" thumb-label="always">
-                                    </v-slider>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols="2">
-                                    <v-text-field v-model="first" label="Character" filled></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-slider
-                                            v-model="max"
-                                            label="Probability"
-                                            thumb-label="always"
-                                    ></v-slider>
-                                </v-col>
-
-                                <v-col>
-                                    <v-slider v-model="max" label="Clarity" thumb-label="always">
-                                    </v-slider>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols="2">
-                                    <v-text-field v-model="first" label="Character" filled></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-slider
-                                            v-model="max"
-                                            label="Probability"
-                                            thumb-label="always"
-                                    ></v-slider>
-                                </v-col>
-
-                                <v-col>
-                                    <v-slider v-model="max" label="Clarity" thumb-label="always">
-                                    </v-slider>
+                                    <i v-if="characterAnnotation.showDelete" class="button-style mdi mdi-delete"
+                                       style="color: darkred"
+                                       v-on:click="deleteFn(characterAnnotation.id)"></i>
+                                    <i v-if="characterAnnotation.showAdd" class="button-style mdi mdi-plus-circle" style="color: dodgerblue"
+                                       v-on:click="addFn(characterAnnotation.id)"></i>
                                 </v-col>
                             </v-row>
                         </v-form>
+
                     </v-col>
                 </v-row>
 
                 <!--                <div class="headline font-weight-light ma-5">Welcome {{ greetedUser }}</div>-->
             </v-card-text>
             <v-card-actions>
-                <v-btn to="/main/profile/view">Previous Image</v-btn>
+<!--                <v-btn to="/main/profile/view">Previous Image</v-btn>-->
                 <v-btn to="/main/profile/edit">Next Image</v-btn>
                 <!--                <v-btn to="/main/profile/password">Change Password</v-btn>-->
             </v-card-actions>
         </v-card>
-
-        <!--    <v-card class="ma-3 pa-3">-->
-        <!--      <v-card-title primary-title>-->
-        <!--        <div class="headline primary&#45;&#45;text">Dashboard</div>-->
-        <!--      </v-card-title>-->
-        <!--      <v-card-text>-->
-        <!--        <div class="headline font-weight-light ma-5">Welcome {{ greetedUser }}</div>-->
-        <!--      </v-card-text>-->
-        <!--      <v-card-actions>-->
-        <!--        <v-btn to="/main/profile/view">View Profile</v-btn>-->
-        <!--        <v-btn to="/main/profile/edit">Edit Profile</v-btn>-->
-        <!--        <v-btn to="/main/profile/password">Change Password</v-btn>-->
-        <!--      </v-card-actions>-->
-        <!--    </v-card>-->
     </v-container>
 </template>
 
 <script lang="ts">
+  import uniqueId from "lodash.uniqueid";
   import { Component, Vue } from "vue-property-decorator";
   import { mainStore } from "@/store";
+  import CharacterForm from "../../components/CharacterForm.vue";
 
-  @Component
+
+
+  @Component({
+    components: {
+      CharacterForm,
+    },
+    data() {
+      console.log(uniqueId);
+      return {
+
+        characterAnnotations: [
+          { id: uniqueId(), character: "", probability: 100, clarity: 100, showDelete: false, showAdd: true },
+        ],
+      };
+    },
+  })
   export default class Dashboard extends Vue {
+    characterAnnotations: any;
+
+    addFn() {
+      console.log(this.characterAnnotations.length)
+      console.log(this.characterAnnotations)
+      this.characterAnnotations[this.characterAnnotations.length-1].showAdd = false;
+      this.characterAnnotations.push({
+        id: uniqueId(),
+        character: "",
+        probability: 100,
+        clarity: 100,
+        showDelete:true,
+        showAdd:true
+      });
+
+    }
+
+    deleteFn(id) {
+      if (this.characterAnnotations.length > 1) {
+        this.characterAnnotations = this.characterAnnotations.filter(item => item.id !== id);
+      }
+      if(this.characterAnnotations.length == 1){
+        this.characterAnnotations[0].showDelete = false
+        this.characterAnnotations[0].showAdd = true
+      }
+    }
+
     get greetedUser() {
       const userProfile = mainStore.userProfile;
       if (userProfile && userProfile.full_name) {
@@ -152,6 +141,11 @@
         background-repeat: no-repeat;
         background-size: 300px 100px;
         /* clip: shape(top, right, bottom, left); NB 'rect' is the only available option */
+    }
+
+    .button-style {
+        font-size: 2em;
+        padding: 10px
     }
 
 </style>
