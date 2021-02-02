7b5fbc858ac00792b3a7a606094d4340836a0f76
kingspp <kingspprathyush@gmail.com>
2021-02-01 20:22:17 -0500

    Added baselines

---
+++ baselines/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+DATA_PATH = PATH+"/data/"
+
+print(PATH)
+
+EPSILON = 1e-8
+
+if DATA_PATH is None:
+    raise Exception('Configure your data folder location in config.py before continuing!')
