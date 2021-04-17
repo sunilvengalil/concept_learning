43268120f99b830bb5e0deb7f9be19cdc57676e8
sunilvengalil-rzt <vengalilsunilkumar@gmail.com>
2021-04-17 15:47:19 +0530

    Parameterize encoded_csv_filename

--- clearn/config/common_path.py
+++ clearn/config/common_path.py
@@ -5,8 +5,7 @@ SPLIT_NAME="Split_1"
 DATASET_NAME = 'mnist'
 
 
-def get_encoded_csv_file(exp_config:ExperimentConfig, epoch:int, train_val="train"):
+def get_encoded_csv_file(exp_config:ExperimentConfig, epoch:int, train_val="train", prefix="z"):
     n_3 = exp_config.num_units[exp_config.num_decoder_layer - 2]
     n_2 = exp_config.num_units[exp_config.num_decoder_layer - 3]
-
-    return f"z_{train_val}_{n_2}_{n_3}_{exp_config.Z_DIM}_epoch_{epoch}.csv"
+    return f"{prefix}_{train_val}_{n_2}_{n_3}_{exp_config.Z_DIM}_epoch_{epoch}.csv"
