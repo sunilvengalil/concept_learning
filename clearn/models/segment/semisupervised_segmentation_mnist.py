13637f9a7f2a527c6e860a4f0210ec7a7fe6e392
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-05-02 15:28:32 +0530

    Suport for more than one hidden layer- segmentation

--- clearn/models/segment/semisupervised_segmentation_mnist.py
+++ clearn/models/segment/semisupervised_segmentation_mnist.py
@@ -219,7 +219,7 @@ class SemiSupervisedSegmenterMnist(VAE):
                                                                                               }
                                                                                    )
                 # print(f"Epoch: {epoch}/{batch}, Nll_loss shape: {nll_loss.shape}, Nll_batch: {nll_batch.shape}")
-                print(f"Epoch: {epoch}/{batch},  Loss: {loss}  Nll_loss : {nll_loss} KLD:{kl_loss} ")
+                # print(f"Epoch: {epoch}/{batch},  Loss: {loss}  Nll_loss : {nll_loss} KLD:{kl_loss} ")
 
                 self.counter += 1
                 self.num_steps_completed = batch + 1
