c38b9dde625649920e8f1ee657edfd76205ef43a
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-05-09 11:39:20 +0530

    Tet code for multiple layers with combination of dense and conv

--- mnist_experiments/un_supervised_classification_net.py
+++ mnist_experiments/un_supervised_classification_net.py
@@ -1,25 +1,50 @@
 import tensorflow as tf
 from clearn.experiments.experiment import initialize_model_train_and_get_features
 
-experiment_name = "Experiment_1"
-root_path = "/Users/sunilv/concept_learning_exp/"
-z_dim_range = [5, 15, 2]
+experiment_name = "Experiment_1_test"
+#root_path = "/Users/sunilv/concept_learning_exp/"
+root_path = "C:/concept_learning_exp/"
+
+z_dim_range = [10, 40, 3]
 learning_rate = 0.001
 num_epochs = 10
 num_runs = 5
-create_split = False
+create_split = True
 completed_z_dims = 0
+z_dim = 20
+run_id =1
 
-for z_dim in range(z_dim_range[0], z_dim_range[1], z_dim_range[2]):
-    for run_id in range(num_runs):
-        initialize_model_train_and_get_features(experiment_name=experiment_name,
-                                                z_dim=z_dim,
-                                                run_id=run_id,
-                                                create_split=create_split,
-                                                num_epochs=num_epochs,
-                                                root_path=root_path,
-                                                learning_rate=learning_rate,
-                                                run_evaluation_during_training=True,
-                                                eval_interval_in_epochs=0.25,
-                                                model_type="VAE")
-        tf.reset_default_graph()
+#for z_dim in range(z_dim_range[0], z_dim_range[1], z_dim_range[2]):
+ #   for run_id in range(num_runs):
+num_units_list_5layer = [[512, 256, 256, 128, 128],
+                         [512, 256, 256, 128, 64],
+                         [512, 256, 128, 128, 64],
+                         [512, 256, 128, 64, 64],
+                         [512, 256, 128, 128, 64],
+                         [512, 256, 128, 128, 64]
+                      ]
+
+strides=[2, 2, 2, 1, 1, 1]
+
+#already tested
+num_units = [512, 256, 128, 64, 32],
+
+initialize_model_train_and_get_features(experiment_name=experiment_name,
+                                        z_dim=z_dim,
+                                        run_id=run_id,
+                                        num_units=num_units_list_5layer[0],
+                                        create_split=create_split,
+                                        num_epochs=num_epochs,
+                                        root_path=root_path,
+                                        learning_rate=learning_rate,
+                                        run_evaluation_during_training=True,
+                                        eval_interval_in_epochs=1,
+                                        model_type="VAE",
+                                        strides=[2,2,2,1,1,1],
+                                        fully_convolutional=False,
+                                        num_dense_layers=2,
+                                        batch_size=4,
+                                        dataset_name="cifar_10",
+                                        split_name="split_1"
+                                        )
+tf.reset_default_graph()
