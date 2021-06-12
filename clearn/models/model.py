c94876afd16a9cb2be82bf0e179898da28033162
sunilvengalil-rzt <vengalilsunilkumar@gmail.com>
2021-06-12 11:17:04 +0530

    Add 1 to checkpoint epoch always

--- clearn/models/model.py
+++ clearn/models/model.py
@@ -74,10 +74,7 @@ class Model(ABC):
             if check_point_epochs is not None:
                 num_training_samples = self.dao.number_of_training_samples // self.exp_config.BATCH_SIZE
                 print("num_training_samples", num_training_samples)
-                if check_point_epochs == 1:
-                    steps = check_point_epochs * num_training_samples + 1
-                else:
-                    steps = check_point_epochs * num_training_samples
+                steps = check_point_epochs * num_training_samples + 1
                 ckpt_name = f"{self._model_name_}.model-{steps}"
             print("ckpt_name", ckpt_name)
             self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
