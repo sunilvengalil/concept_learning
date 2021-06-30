c9fd794035d1c4a53565aded38ce960a3e6e1b94
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-06-30 19:11:47 +0530

    Code to generate operator dataset

--- clearn/dao/mnist_with_concepts.py
+++ clearn/dao/mnist_with_concepts.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 import numpy as np
 import os
 import gzip
@@ -12,66 +14,113 @@ import pandas as pd
 MAP_FILE_NAME = "manually_generated_concepts.json"
 MAX_IMAGES_TO_DISPLAY = 12
 
-OPERATORS = ["IDENTITY", "PUT_BOTH"]
+OPERATORS = ["IDENTITY", "VERTICAL_CONCATENATE"]
 
 
-def apply_operator(concepts_to_use_1,
-                   concepts_to_use_2,
-                   operators_to_use,
+class Operator:
+    def __init__(self, num_samples_required, operator):
+        self.index = 0
+        self.num_samples_required = num_samples_required
+        self.operator = operator
+        if OPERATORS[operator] == "VERTICAL_CONCATENATE":
+            self.concept_list = [12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+        elif OPERATORS[operator] == "IDENTITY":
+            self.concept_list = [10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+        self.concepts_to_use_1 = np.random.choice(self.concept_list, num_samples_required)
+        self.concepts_to_use_2 = np.random.choice(self.concept_list, num_samples_required)
+
+    def reset_index(self):
+        self.index = 0
+
+    def get_next_images(self):
+        c1, c2 = self.concepts_to_use_1[self.index], self.concepts_to_use_2[self.index]
+        self.index += 1
+        if self.index == self.concepts_to_use_1.shape[0]:
+            self.index = 0
+        return c1, c2
+
+    @abstractmethod
+    def apply_operation(self, concept_1_image, concept_2_image):
+        pass
+
+
+class IdentityOperator(Operator):
+    def __init__(self, num_samples_required):
+        super().__init__(num_samples_required,
+                         0)
+
+    def apply_operation(self, concept_1_image, concept_2_image=None):
+        masked_image = np.zeros([
+            1,
+            28,
+            28,
+            1
+        ])
+        from_index_h = (28 - concept_1_image.shape[0]) // 2
+        to_index_h = from_index_h + concept_1_image.shape[0]
+        from_index_v = (28 - concept_1_image.shape[1]) // 2
+        to_index_v = from_index_h + concept_1_image.shape[1]
+        masked_image[0, from_index_h:to_index_h, from_index_v:to_index_v, 0] = concept_1_image
+        return masked_image
+
+
+class ConcatenateVerticalOperator(Operator):
+    def __init__(self, num_samples_required):
+        super().__init__(num_samples_required,
+                         1)
+
+    def apply_operation(self, concept_1_image, concept_2_image):
+        masked_image = np.zeros([
+            1,
+            28,
+            28,
+            1
+        ])
+        if concept_1_image.shape[1] < concept_2_image.shape[1]:
+            num_zeropadding = concept_2_image.shape[1] - concept_1_image.shape[1]
+            zero_padding_image = np.zeros((concept_1_image.shape[0], num_zeropadding))
+            concept_1_image = np.hstack([zero_padding_image, concept_1_image])
+        elif concept_2_image.shape[1] < concept_1_image.shape[1]:
+            num_zeropadding = concept_1_image.shape[1] - concept_2_image.shape[1]
+            zero_padding_image = np.zeros((concept_2_image.shape[0], num_zeropadding))
+            concept_2_image = np.hstack([zero_padding_image, concept_2_image])
+        combined = np.vstack([concept_1_image, concept_2_image])
+        from_index_h = (28 - combined.shape[0]) // 2
+        to_index_h = from_index_h + combined.shape[0]
+        from_index_v = (28 - combined.shape[1]) // 2
+        to_index_v = from_index_h + combined.shape[1]
+        masked_image[0, from_index_h:to_index_h, from_index_v:to_index_v, 0 ] = combined
+        return masked_image
+
+
+def apply_operator(operators_to_use,
                    key_image_concept_map,
                    key_to_label_map,
                    label_start):
 
-    derived_images = np.zeros((concepts_to_use_1.shape[0], 28, 28, 1))
-    derived_labels = np.zeros((concepts_to_use_1.shape[0] ), np.int8)
-    image_index = 0
-    image_shape = [28, 28]
-    height = image_shape[0]
-    width = image_shape[0]
+    derived_images = np.zeros((operators_to_use.shape[0], 28, 28, 1))
+    derived_labels = np.zeros((operators_to_use.shape[0] ), np.int8)
     print(key_to_label_map)
-    print("Total number of images to generate", concepts_to_use_1.shape[0])
-    for concept_to_use_1, concept_to_use_2, operator_to_use in zip(concepts_to_use_1, concepts_to_use_2, operators_to_use):
+    print("Total number of images to generate", operators_to_use.shape[0])
+    images_for_operator = [IdentityOperator(operators_to_use.shape[0]),
+                           ConcatenateVerticalOperator(operators_to_use.shape[0])]
+    concepts_1_to_use = np.zeros(operators_to_use.shape[0])
+    concepts_2_to_use = np.zeros(operators_to_use.shape[0])
+
+    for image_index, operator_to_use in enumerate(operators_to_use):
+        concept_to_use_1, concept_to_use_2 = images_for_operator[operator_to_use].get_next_images()
+        concepts_1_to_use[image_index] = concept_to_use_1
+        concepts_2_to_use[image_index] = concept_to_use_2
+
         image_concept_1:ImageConcept = key_image_concept_map[key_to_label_map[concept_to_use_1]]
-        h_extend_1 = image_concept_1.h_extend
-        v_extend_1 = image_concept_1.v_extend
-        if len(v_extend_1) == 0:
-            v_extend_1 = [0, height]
-        if len(h_extend_1) == 0:
-            h_extend_1 = [0, width]
-
-        digit_image_1 = np.squeeze(np.asarray(image_concept_1.digit_image))
         image_concept_2:ImageConcept = key_image_concept_map[key_to_label_map[concept_to_use_2]]
-        h_extend_2 = image_concept_1.h_extend
-        v_extend_2 = image_concept_1.v_extend
-        if len(v_extend_2) == 0:
-            v_extend_2 = [0, height]
-        if len(h_extend_2) == 0:
-            h_extend_2 = [0, width]
-
-        digit_image_2 = np.squeeze(np.asarray(image_concept_2.digit_image))
-        cropped_1 = digit_image_1[ v_extend_1[0]:v_extend_1[1], h_extend_1[0]:h_extend_1[1]]
-        cropped_2 = digit_image_2[ v_extend_2[0]:v_extend_2[1], h_extend_2[0]:h_extend_2[1]]
-
-        masked_image = np.zeros([
-            1,
-            image_shape[0],
-            image_shape[1],
-            1
-        ])
-
-        if operator_to_use == 0:
-            masked_image[0, v_extend_1[0]:v_extend_1[1], h_extend_1[0]:h_extend_1[1], 0] = cropped_1
-            derived_images[image_index] = masked_image
-        if operator_to_use == 1:
-            masked_image[0, v_extend_1[0]:v_extend_1[1], h_extend_1[0]:h_extend_1[1], 0] = cropped_1
-            masked_image[0, v_extend_2[0]:v_extend_2[1], h_extend_2[0]:h_extend_2[1], 0] = cropped_2
-        derived_images[image_index] = masked_image
+        cropped_1 = image_concept_1.get_cropped_and_stripped()
+        cropped_2 = image_concept_2.get_cropped_and_stripped()
+        derived_images[image_index] = images_for_operator[operator_to_use].apply_operation(cropped_1, cropped_2)
         derived_labels[image_index] = label_start + operator_to_use
-        image_index += 1
         if image_index % 1000 == 0:
-            print(f"Generated {image_index} out of {concepts_to_use_1.shape[0]} images")
-
-    return derived_images, derived_labels
+            print(f"Generated {image_index} out of {operators_to_use.shape[0]} images")
+    return concepts_1_to_use, concepts_2_to_use, derived_images, derived_labels
 
 
 def get_key(digit, h_extend, v_extend):
@@ -269,14 +318,8 @@ class MnistConceptsDao(IDao):
         for k in concepts_dict.keys():
             print(k, [k1 for k1 in concepts_dict[k][0].keys()])
         operator_to_use = np.random.choice([0, 1],num_images_per_concept * len(OPERATORS))
-        concepts_to_use_1 = np.random.choice(list(self.key_to_label_map.keys()),
-                                             num_images_per_concept * len(OPERATORS))
-        concepts_to_use_2 = np.random.choice(list(self.key_to_label_map.keys()),
-                                             num_images_per_concept * len(OPERATORS))
 
-        derived_images, derived_labels = apply_operator(concepts_to_use_1,
-                                                        concepts_to_use_2,
-                                                        operator_to_use,
+        concepts_to_use_1, concepts_to_use_2, derived_images, derived_labels = apply_operator(operator_to_use,
                                                         self.key_to_image_concept_map,
                                                         self.key_to_label_map,
                                                         self.num_original_classes + self.num_concepts_label_generated)
