68f6d14bde6c1c501f0888ce78458ce5161fda5e
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-06-30 16:04:59 +0530

    Code to generate operator dataset

--- clearn/analysis/__init__.py
+++ clearn/analysis/__init__.py
@@ -128,7 +128,7 @@ class ImageConcept:
             v_extend = [0, 28]
         if len(h_extend) == 0:
             h_extend = [0, 28]
-        return self.digit_image[v_extend[0]:v_extend[1], h_extend[0]:h_extend[1]]
+        return np.asndarray(self.digit_image)[v_extend[0]:v_extend[1], h_extend[0]:h_extend[1]]
 
     def get_cropped_and_stripped(self):
         cropped = self.get_cropped_image()
