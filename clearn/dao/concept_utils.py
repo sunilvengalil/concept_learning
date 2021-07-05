a061c3ae9021d13151d021456db16ab1e57a0d07
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-07-05 09:56:33 +0530

    Changes for translate image

--- clearn/dao/concept_utils.py
+++ clearn/dao/concept_utils.py
@@ -143,7 +143,8 @@ def segment_single_image_with_multiple_slices(image,
 
         cropped = image[ v_extend[0]:v_extend[0] + v_extend[1], h_extend[0]:h_extend[0] + h_extend[1]]
 
-        cropped_and_stripped, _, _ = ImageConcept.tight_bound_h(ImageConcept.tight_bound_v(cropped))
+        v_im, _ = ImageConcept.tight_bound_v(cropped)
+        cropped_and_stripped, _= ImageConcept.tight_bound_h(v_im)
         if translate_image:
             top = randint(0, height - cropped_and_stripped.shape[0])
             left = randint(0, width - cropped_and_stripped.shape[1])
