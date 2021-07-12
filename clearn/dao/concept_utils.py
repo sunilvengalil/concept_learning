85f48a7c033ce1232c7340f5f3f9e73e34d5ce86
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-07-12 20:46:10 +0530

    Exclude blank images

--- clearn/dao/concept_utils.py
+++ clearn/dao/concept_utils.py
@@ -133,9 +133,9 @@ def segment_single_image_with_multiple_slices(image,
         h_im, _ = ImageConcept.tight_bound_h(cropped)
         cropped_and_stripped, _= ImageConcept.tight_bound_v(h_im)
         if digit_location_key == "4_17_23_17_28":
-            print(np.sum(cropped_and_stripped))
+            print(digit_location_key, np.sum(cropped_and_stripped))
         if np.sum(cropped_and_stripped) < 10:
-            print(f"Skipping {digit_location_key} ")
+            print(f"Skipping {digit_location_key} np.sum(cropped_and_stripped) ")
             continue
 
         if translate_image:
