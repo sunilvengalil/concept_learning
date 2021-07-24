2539175e1e199cfb8b97c09e671bae1beed25c38
sunilvengalil <vengalilsunilkumar@gmail.com>
2021-07-24 13:46:21 +0530

    added debug logs

--- clearn/models/vae.py
+++ clearn/models/vae.py
@@ -136,7 +136,9 @@ class VAE(GenerativeModel):
                                                          self.out,
                                                          reduction=tf.compat.v1.losses.Reduction.NONE
                                                          )
+            print("Mll", mll.shape)
             self.marginal_likelihood = -tf.compat.v1.reduce_mean(mll, axis=(1, 2, 3))
+            print("after reduction", self.marginal_likelihood.shape)
         self.neg_loglikelihood = -tf.reduce_mean(self.marginal_likelihood)
 
         kl = 0.5 * tf.reduce_sum(tf.square(self.mu) +
