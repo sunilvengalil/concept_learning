3d548c4a7a210d6ec125651517287e7d8ba6b9bf
sunilvengalil-rzt <vengalilsunilkumar@gmail.com>
2021-08-29 09:15:02 +0530

    Fixing issues in fmnist dao

--- clearn/dao/fmnist.py
+++ clearn/dao/fmnist.py
@@ -14,7 +14,7 @@ class FashionMnistDao(IDao):
 
     @property
     def number_of_training_samples(self):
-        return 50000 - self.num_validation_samples
+        return 60000 - self.num_validation_samples
 
     @property
     def number_of_testing_samples(self):
@@ -78,7 +78,7 @@ class FashionMnistDao(IDao):
         #                          28 * 28)
 
         data = pd.read_csv(data_dir + "/fashion-mnist_train.csv")
-        x = data[x_columns].values.reshape((60000, 28, 28, 1))
+        x = data[x_columns].values.reshape((59872, 28, 28, 1))
 
 
         #data = self.extract_data(data_dir + '/train-labels-idx1-ubyte.gz', self.number_of_training_samples, 8, 1)
