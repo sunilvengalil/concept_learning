21c7f54cc8b6f9e52b5d275bbb4e12c65fb47469
sunilvengalil <vengalilsunilkumar@gmail.com>
2025-01-11 14:57:27 +0530

    Fix the bug when num_dense layers is zero.

--- clearn/models/architectures/custom/tensorflow_graphs.py
+++ clearn/models/architectures/custom/tensorflow_graphs.py
@@ -204,6 +204,7 @@ def deconv_n_layer(model, z,  out_channels, reuse=False):
     with tf.compat.v1.variable_scope("decoder", reuse=reuse):
         if model.exp_config.activation_hidden_layer == "RELU":
             # Add dense layers
+            model.dense_out = z
             if model.exp_config.num_dense_layers > 0:
                 model.decoder_dense_dict = dict()
                 layer_num = 0
