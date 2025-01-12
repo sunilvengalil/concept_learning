51c907f695e37b5cd5a73f9833714682925bc449
sunilvengalil <vengalilsunilkumar@gmail.com>
2025-01-12 13:29:12 +0530

    Fix the bug when num_dense layers is zero.

--- clearn/models/architectures/custom/tensorflow_graphs.py
+++ clearn/models/architectures/custom/tensorflow_graphs.py
@@ -235,7 +235,7 @@ def deconv_n_layer(model, z,  out_channels, reuse=False):
         print("Layer number", layer_num)
         num_features_index = len(n_units) - (layer_num + 1)
         num_features = n_units[num_features_index]
-        image_size = model.image_sizes[num_features_index]
+        image_size = model.image_sizes[num_features_index + 1 ]
         num_units = image_size[0] * image_size[1] * image_size[2]
         if model.exp_config.log_level == logging.DEBUG:
             print(num_features_index, num_features, image_size, num_units)
