3163693b22879d76a921ad077ce5e193b64add98
svengalil <vengalilsunilkumar@gmail.com>
2026-05-01 18:33:10 +0530

    Print loss during training. Clean up code for concept loss

--- clearn/analysis/encode_decode.py
+++ clearn/analysis/encode_decode.py
@@ -156,34 +156,25 @@ def encode_and_get_features(model: GenerativeModel,
     mus = np.zeros([len(images), z_dim])
     sigmas = np.zeros([len(images), z_dim])
     latent_vectors = np.zeros([len(images), z_dim])
-    dense2_ens = np.zeros([len(images), model.exp_config.num_units[2]])
-    reshapeds = []
-    conv2_ens = []
-    conv1_ens = []
+    features = []
 
     for batch_num in range(num_batches):
-        mu, sigma, z, dense2_en, reshaped, conv2_en, conv1_en = model.encode_and_get_features(
+        hidden_feature_names, mu, sigma, z, feature = model.encode_and_get_features(
             images[batch_num * batch_size: (batch_num + 1) * batch_size])
         mus[batch_num * batch_size: (batch_num + 1) * batch_size] = mu
         sigmas[batch_num * batch_size: (batch_num + 1) * batch_size] = sigma
         latent_vectors[batch_num * batch_size: (batch_num + 1) * batch_size] = z
-        dense2_ens[batch_num * batch_size: (batch_num + 1) * batch_size] = dense2_en
-        reshapeds.append(reshaped)
-        conv2_ens.append(conv2_en)
-        conv1_ens.append(conv1_en)
+        features.append(feature)
 
     left_out = num_images % batch_size
     if left_out > 0:
         feature_dimension = [batch_size, model.dao.image_shape[0], model.dao.image_shape[1], model.dao.image_shape[2]]
         last_batch = np.zeros(feature_dimension)
         last_batch[0:left_out] = images[num_batches * batch_size:]
-        mu, sigma, z, dense2_en, reshaped, conv2_en, conv1_en = model.encode_and_get_features(last_batch)
+        hidden_feature_names, mu, sigma, z, feature = model.encode_and_get_features(last_batch)
         mus[num_batches * batch_size:] = mu[0:left_out]
         sigmas[num_batches * batch_size:] = sigma[0:left_out]
         latent_vectors[num_batches * batch_size:] = z[0:left_out]
-        dense2_ens[num_batches * batch_size:] = dense2_en[0:left_out]
-        reshapeds.append(reshaped)
-        conv2_ens.append(conv2_en)
-        conv1_ens.append(conv1_en)
+        features.append(feature)
 
-    return mus, sigmas, latent_vectors, dense2_ens, reshapeds, conv2_ens, conv1_ens
+    return hidden_feature_names, mus, sigmas, latent_vectors, features
